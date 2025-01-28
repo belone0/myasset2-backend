30cca7413db83e27fa86f290224cbb3d51729686
belone0 <belone00@gmail.com>
2025-01-28 11:10:49 -0300

    Atualiza a porta exposta no Dockerfile, desabilita SSL em produção e permite origens dinâmicas no CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "http://144.22.172.99:81" # Update with your Vue app's URL
+    origins "http://localhost:5173", "http://144.22.172.99:*" # Update with your Vue app's URL
     resource "*",
              headers: :any,
              methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
